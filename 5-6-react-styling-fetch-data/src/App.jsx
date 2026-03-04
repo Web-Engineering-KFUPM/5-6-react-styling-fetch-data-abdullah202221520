4b4daf3e33f362eae9054113bdec4afdac1ea1bc
abdullah202221520 <s202221520@kfupm.edu.sa>
2026-03-04 12:18:09 +0300

    task2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -203,7 +203,24 @@ export default function App() {
      Implement fetch logic inside this useEffect.
      ========================================================= */
   useEffect(() => {
-    // TODO 2.1: Implement fetching users here (see lab instructions)
+      const fetchUsers = async() => {
+         try {
+            setLoading(true);
+            setError(null);
+            fetch("https://jsonplaceholder.typicode.com/users");
+            if (!response.ok) {
+               throw new Error("Failed to fetch users");
+            }
+            const data = await response.json();
+            setUsers(data);
+            setFilteredUsers(data);
+         }
+         catch (err) {
+            setError(err.message);
+         } finally {
+            setLoading(false);
+         }
+      }
   }, []);
 
   /* =========================================================
@@ -214,7 +231,14 @@ export default function App() {
      Dependency array MUST be: [searchTerm, users]
      ========================================================= */
   useEffect(() => {
-    // TODO 2.2: Implement filtering users here (see lab instructions)
+   if (searchTerm === "") {
+    setFilteredUsers(users);
+  } else {
+    const filtered = users.filter((user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  }
   }, [searchTerm, users]);
 
   // Modal handlers (already complete)
